a87f65cb35ba55c4c36109da1077f372547b0eb9
Jukka Nieminen <aa2233@student.jamk.fi>
2022-10-19 18:31:01 +0300

    Added additional variable

--- FirstProgram.py
+++ FirstProgram.py
@@ -5,7 +5,8 @@ import qrcode
 
 addTextToQRCode = input('Add a text to qrgenerator: ')
 
+addNameToFile = input('Please name your image name: ')
 
 img = qrcode.make(addTextToQRCode)
 
-img.save("testing.png")
+img.save(addNameToFile)
